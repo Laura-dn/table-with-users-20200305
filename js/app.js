92cc982d079384f53ede0e36441f323dadf49cc4
Laura-dn <56314608+Laura-dn@users.noreply.github.com>
2020-05-13 18:02:38 +0300

    update files

--- js/app.js
+++ js/app.js
@@ -1,28 +1,77 @@
-function load(url, callback){
-    if(!url || !callback){
-        return false;
+function load() {
+    let numUsers = Number(document.querySelector("#userNum").value),
+        table = document.querySelector("table");
+
+    if(isNaN(numUsers) || numUsers == 0) {
+        numUsers = 1;
     }
-    const aja = new XMLHttpRequest();
-    aja.onload = showUser;
-    aja.open('GET', url);
-    aja.send();
+
+    if(numUsers > 5000) {
+        numUsers = 5000;
+    }
+
+    let url = "https://randomuser.me/api/?results=" + numUsers;
+
+    const XHR = new XMLHttpRequest();
+    XHR.onload = showUser;
+    XHR.open('GET', url);
+    XHR.send();
+    
+    if(table != null) {
+        table.remove();
+    }
+    
     return true;
 }
 
-function showUser(){
-    console.log(JSON.parse(this.responseText));
+function showUser() {
+    let arr = JSON.parse(this.responseText).results,
+        table = document.createElement("table"),
+        arrHead = ["#","Name","Email","Age","Address","Phone"],
+        section = document.querySelector("section");
+
+    for(let i = 0; i <= arr.length; i++) {
+        let tr = document.createElement("tr");
+
+        if(i == 0) {
+            for(let j = 0; j < arrHead.length; j++) {
+                let th = document.createElement("th");
+                th.innerHTML = arrHead[j];
+                tr.append(th);
+            }
+        } else {
+            tr.innerHTML = `<td>${i}</td>
+            <td>${arr[(i-1)].name.title} ${arr[(i-1)].name.first} ${arr[(i-1)].name.last}</td>
+            <td>${arr[(i-1)].email}</td>
+            <td>${arr[(i-1)].dob.age}</td>
+            <td>${arr[(i-1)].location.country}, ${arr[(i-1)].location.city}, ${arr[(i-1)].location.postcode} 
+            ${arr[(i-1)].location.state}, ${arr[(i-1)].location.street.number} ${arr[(i-1)].location.street.name}</td>
+            <td>${arr[(i-1)].phone}</td>`;
+
+
+            // for(let j = 0; j < arrHead.length; j++) {
+            //     let td = document.createElement("td");
+            //     td.innerHTML = 
+            // }
+        }
+
+        table.append(tr);
+    }
+
+    section.append(table);
 }
 
+//('https://randomuser.me/api/', showUser)- https://randomuser.me/api/?results=10
 
 /**Функция обработчик события load объекта AJAX
  * arguments : none
  * return : none
  */
-function showUser(){
-    console.log('Операция получения данных с сервера завершена');
-    const str = dom.cards.innerHTML;
-    dom.cards.innerHTML = renderUser(JSON.parse(this.responseText).results[0]) + str;
-}
+// function showUser(){
+//     console.log('Операция получения данных с сервера завершена');
+//     const str = dom.cards.innerHTML;
+//     dom.cards.innerHTML = renderUser(JSON.parse(this.responseText).results[0]) + str;
+// }
 
 
 /**Функция генерации карточки в виде HTML-строки
@@ -31,20 +80,14 @@ function showUser(){
  * return:
  *  string : html-строку с карточкой
  */
-function renderUser(user){
-    return `<div class="card d-inline-block m-2" style="width: 18rem;">
-    <img src="${user.picture.large}" class="card-img-top" alt="user-pic">
-    <div class="card-body">
-        <h5 class="card-title">${user.name.title} ${user.name.first} ${user.name.last}</h5>
-        <p class="card-text">${user.dob.age} ${user.gender}, ${user.nat}</p>
-    </div>
-</div>`;
-}
+// function renderUser(user){
+//     return `<div class="card d-inline-block m-2" style="width: 18rem;">
+//     <img src="${user.picture.large}" class="card-img-top" alt="user-pic">
+//     <div class="card-body">
+//         <h5 class="card-title">${user.name.title} ${user.name.first} ${user.name.last}</h5>
+//         <p class="card-text">${user.dob.age} ${user.gender}, ${user.nat}</p>
+//     </div>
+// </div>`;
+// }
 
-const dom = {
-    cards : document.querySelector('.cards'),
-    btn   : document.querySelector('.btn-add-user')
-};
-
-/**Вешаем обработчик клика на кнопку*/
-dom.btn.addEventListener('click', ()=>load('https://randomuser.me/api/', showUser));
+document.querySelector("#btn").addEventListener("click", load);
